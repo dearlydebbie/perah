ea441b5f0732330accf3cef39065a0bd1b896bb5
Deborah <debbieadeoye1@gmail.com>
2022-05-30 18:55:21 +0100

    product

--- src/component/Products.jsx
+++ src/component/Products.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
@@ -46,10 +47,10 @@ const Products = () => {
 		);
 	};
 
-  const filterProduct = (cat) => {
-    const updatedList = data.filter((x)=>x.category === cat);
-    setFilter(updatedList);
-  }
+	const filterProduct = (cat) => {
+		const updatedList = data.filter((x) => x.category === cat);
+		setFilter(updatedList);
+	};
 
 	const ShowProducts = () => {
 		return (
@@ -90,21 +91,24 @@ const Products = () => {
 					return (
 						<>
 							<div className="col-md-3 mb-4">
-								<div class="card h-100 text-center p-4" key={product.id}>
+								<div className="card h-100 text-center p-4" key={product.id}>
 									<img
-										class="card-img-top"
+										className="card-img-top"
 										src={product.image}
 										alt={product.title}
 										height="250px"
 									/>
-									<div class="card-body">
-										<h5 class="card-title mb-0">
+									<div className="card-body">
+										<h5 className="card-title mb-0">
 											{product.title.substring(0, 12)}...
 										</h5>
-										<p class="card-text lead fw-bold">${product.price}</p>
-										<a href="#" class="btn btn-outline-dark">
+										<p className="card-text lead fw-bold">${product.price}</p>
+										<NavLink
+											to={`/products/${product.id}`}
+											className="btn btn-outline-dark"
+										>
 											Buy Now
-										</a>
+										</NavLink>
 									</div>
 								</div>
 							</div>
